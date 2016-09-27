6a8f2d16eac50713ca05c66b6a344b949615b30f
Emil Hernqvist <e.g.hernqvist@gmail.com>
2016-09-27 09:19:32 +0200

    implemented ui.select

--- app/assets/javascripts/godmin/godmin-controller.js
+++ app/assets/javascripts/godmin/godmin-controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('godmin', ['ngMaterial', 'md.data.table']);
+var app = angular.module('godmin', ['ngMaterial', 'md.data.table', 'ui.select']);
 
 app.config(['$mdThemingProvider', '$mdIconProvider', '$sceDelegateProvider', function($mdThemingProvider, $mdIconProvider, $sceDelegateProvider) {
 
